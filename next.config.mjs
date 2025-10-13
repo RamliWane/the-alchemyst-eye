54413d1172a02e8a71c4845d6c111bfc9be48025
chaali03 <zaidan.ali2411@gmail.com>
2025-10-13 21:41:31 +0700

    fix preview script and Turbopack root

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+};
 
 export default nextConfig;
